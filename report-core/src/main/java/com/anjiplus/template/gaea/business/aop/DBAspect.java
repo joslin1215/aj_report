38b343c54d94f87d2aa452aad56174fbfada7b8a
linbo <linbo123123>
2022-06-01 14:06:36 +0800

    系统集成

--- report-core/src/main/java/com/anjiplus/template/gaea/business/aop/DBAspect.java
+++ report-core/src/main/java/com/anjiplus/template/gaea/business/aop/DBAspect.java
@@ -1,13 +1,19 @@
 package com.anjiplus.template.gaea.business.aop;
 
 import com.baomidou.dynamic.datasource.toolkit.DynamicDataSourceContextHolder;
+import com.ruijie.jeef.core.util.MybatisPlusContext;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author 林波
@@ -15,7 +21,10 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Slf4j
-public class DatasourceAspect {
+public class DBAspect {
+
+    @Resource
+    HttpServletRequest request;
 
     @Value("${ajreport.datasource.default:aj-report}")
     String defaultDatasource;
@@ -29,6 +38,15 @@ public class DatasourceAspect {
     public void before() {
         log.debug("切换数据源：{}", defaultDatasource);
         DynamicDataSourceContextHolder.push(defaultDatasource);
+        MybatisPlusContext.createdByField("createBy");
+        MybatisPlusContext.createdDateField("createTime");
+        MybatisPlusContext.updatedByField("updateBy");
+        MybatisPlusContext.updatedDateField("updateTime");
+        MybatisPlusContext.fillVersion(true);
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            MybatisPlusContext.fillUser(String.valueOf(session.getAttribute("account")));
+        }
     }
 
     @After("pointcut()")
